0b5c0c1a5007c01fdea9aa70b18e7e753c9b19d2
Lisa <lisa4986@googlemail.com>
2020-06-04 15:58:55 +0200

    exported xls file to csv file; edited main.py; deleted online-created-file.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("hello world")
-print("hello world die 2.")
-print("hi")
+import csv
+with open("vocabulary.csv", newline='', encoding="utf-8") as file:
+    csv_file = csv.reader(file, delimiter=",")
+    for line in csv_file:
+        print(line)
