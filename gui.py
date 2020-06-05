28488adbefcb0968e76408c5644f2890603007c4
lisa4986 <lisa4986@googlemail.com>
2020-06-05 18:47:54 +0200

    added pandas.py

---
+++ gui.py
@@ -0,0 +1,19 @@
+# Python Chapter 16 - Part 187 ff
+import ipywidgets as widgets
+from IPython.display import display
+
+widgets.Button(description="Push me")
+widgets.Text(description="My text", value"123")
+widgets.Checkbox(description"Description" value=False)
+
+widgets.RadioButtons(
+    options=['A','B','C'],
+    description='Which letter?',
+    disabled=False
+)
+
+widgets.Dropdown(
+    options=['A','B','C']
+    description='Which letter?',
+    disabled=False
+)
