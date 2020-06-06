a056ae27d75272539fa023045a4a63efe4c586a1
Lisa <lisa4986@googlemail.com>
2020-06-06 11:33:30 +0200

    changed file name from pandas.py zu pandas-file.py

---
+++ pandas_file.py
@@ -0,0 +1,12 @@
+import pandas as pd
+df = pd.read_csv("vocabulary.csv")
+df
+## output first 5 rows
+## df.head()
+## length of list
+## len(df)
+## column
+## df["English"]
+## row - index 0
+## df.iloc[0]
+# Python Chapter 18 - Part 231
